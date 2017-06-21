fbda43fcec44504d566436674c141d3a784a16f6
Kenneth D. Evensen <kevensen@redhat.com>
2017-06-21 19:22:09 -0400

    adding custom attribute mixin to container project

--- lib/miq_automation_engine/service_models/miq_ae_service_container_project.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_container_project.rb
@@ -1,5 +1,7 @@
 module MiqAeMethodService
   class MiqAeServiceContainerProject < MiqAeServiceModelBase
+    require_relative "mixins/miq_ae_service_custom_attribute_mixin"
+    include MiqAeServiceCustomAttributeMixin
     expose :ext_management_system,  :association => true
     expose :container_groups,       :association => true
   end
